32a7754fc393c52dc89d737affbe371f4fe56323
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-08-03 12:02:09 -0400

    updated functions

--- src/thirdfromend/thirdfromend.js
+++ src/thirdfromend/thirdfromend.js
@@ -1,4 +1,4 @@
-const LinkedList = require("../lib/linkedList");
+const LinkedList = require('../lib/linkedList');
 
 /**
  * Return the third element from the end of the linked list.
@@ -9,6 +9,35 @@ const LinkedList = require("../lib/linkedList");
  * null if list length < 3
  *
  */
-function thirdFromEnd(list) {}
+function thirdFromEnd (list) {
+  if (list.length === 0) {
+    return null;
+  }
+
+  let pointer1 = list.head;
+  let i = 0;
+  while (i < 2 && pointer1.next) {
+    // Set pointer1 to its next pointer
+    pointer1 = pointer1.next;
+    // Increment i
+    i++;
+  }
+
+  if (i < 2) {
+    // If i is less than 2, it means there are not enough nodes in the list
+    // to find the third node from the end, so return null.
+    return null;
+  }
+
+  let pointer2 = list.head;
+  while (pointer1.next) {
+    // Move both pointers until pointer1 reaches the last node
+    // and pointer2 will be pointing to the third node from the end.
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
+
+  return pointer2;
+}
 
 module.exports = thirdFromEnd;
